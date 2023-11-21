e862c34f8ab4a668aa252009c4d0ed44eae0e89d
kohsaka <>
2023-11-21 17:34:54 +0900

    test code

---
+++ poke-poke-dexTests/DetailViewModel_Tests.swift
@@ -0,0 +1,9 @@
+//
+//  DetailViewModelTests.swift
+//  poke-poke-dexTests
+//  
+//  Created by kohsaka on 2023/11/21
+//  
+//
+
+import Foundation
--- poke-poke-dexTests/ListViewModel_Tests.swift
+++ poke-poke-dexTests/ListViewModel_Tests.swift
@@ -8,7 +8,7 @@
 import XCTest
 @testable import poke_poke_dex
 
-final class poke_poke_dexTests: XCTestCase {
+final class ListViewModel_Tests: XCTestCase {
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
